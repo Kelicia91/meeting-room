33219c848c6c1889589325763cb8ba39c8da971a
Kelicia91 <kelicia91@gmail.com>
2018-10-14 04:20:59 +0900

    add : import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO meeting_room (id, name) VALUES (1, 'A');
+INSERT INTO meeting_room (id, name) VALUES (2, 'B');
+INSERT INTO meeting_room (id, name) VALUES (3, 'C');
+INSERT INTO meeting_room (id, name) VALUES (4, 'D');
+INSERT INTO meeting_room (id, name) VALUES (5, 'E');
+INSERT INTO meeting_room (id, name) VALUES (6, 'F');
