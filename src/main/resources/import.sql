ec4180533587394fe3b56b4732777119dc44003e
Kelicia91 <kelicia91@gmail.com>
2018-10-14 18:16:07 +0900

    chore : user 도메인 삭제에 따라 sample data 수정

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,3 +4,5 @@ INSERT INTO meeting_room (id, name) VALUES (3, 'C');
 INSERT INTO meeting_room (id, name) VALUES (4, 'D');
 INSERT INTO meeting_room (id, name) VALUES (5, 'E');
 INSERT INTO meeting_room (id, name) VALUES (6, 'F');
+
+INSERT INTO reservation (id, username, meeting_room_id, date, start_time, end_time) VALUES (1, 'alice', 1, CURRENT_DATE(), '10:00:00', '10:30:00');
