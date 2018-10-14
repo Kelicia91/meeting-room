16c4a9ab313f35101d4f3b94eb2d9804120476e2
Kelicia91 <kelicia91@gmail.com>
2018-10-15 07:47:45 +0900

    fix : REST API ('/api/meeting-room' -> '/api/meeting-rooms')

--- src/main/java/assignment/web/ApiMeetingRoomController.java
+++ src/main/java/assignment/web/ApiMeetingRoomController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/meeting-room")
+@RequestMapping("/api/meeting-rooms")
 public class ApiMeetingRoomController {
 
     @Autowired
