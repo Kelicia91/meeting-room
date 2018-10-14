1de4531dfddfc7c7ecff8a71a37ef7d6913c14ef
Kelicia91 <kelicia91@gmail.com>
2018-10-15 01:51:36 +0900

    chore : 중복코드제거

--- src/test/java/assignment/domain/ReservationTest.java
+++ src/test/java/assignment/domain/ReservationTest.java
@@ -1,5 +1,6 @@
 package assignment.domain;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.time.LocalDate;
@@ -9,66 +10,48 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ReservationTest {
 
+    private String username;
+    private MeetingRoom meetingRoom;
+    private LocalDate date;
+    private LocalTime startTime;
+
+    @Before
+    public void setUp() throws Exception {
+        username = "bob";
+        meetingRoom = new MeetingRoom("A");
+        date = LocalDate.now();
+        startTime = LocalTime.of(10, 0);
+    }
+
     @Test
     public void isValidMinute() {
-        String username = null;
-        MeetingRoom meetingRoom = null;
-        LocalDate date = null;
-        LocalTime startTime = LocalTime.of(0, 0); // DateTimeException
-        LocalTime endTime = LocalTime.of(0, 30);
+        LocalTime endTime = startTime.plusMinutes(30);
         Reservation reservation = new Reservation(username, meetingRoom, date, startTime, endTime);
         assertThat(reservation).isNotNull();
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void isValidMinute_not_0_and_not_30() {
-        String username = null;
-        MeetingRoom meetingRoom = null;
-        LocalDate date = null;
-        LocalTime startTime = LocalTime.of(0, 1);
         LocalTime endTime = LocalTime.of(0, 1);
         new Reservation(username, meetingRoom, date, startTime, endTime);
     }
 
     @Test
-    public void isEndTimeLaterThanStartTime_same_hour_diff_min() {
-        String username = null;
-        MeetingRoom meetingRoom = null;
-        LocalDate date = null;
-        LocalTime startTime = LocalTime.of(0, 0);
-        LocalTime endTime = LocalTime.of(0, 30);
-        Reservation reservation = new Reservation(username, meetingRoom, date, startTime, endTime);
-        assertThat(reservation).isNotNull();
-    }
-
-    @Test
-    public void isEndTimeLaterThanStartTime_diff_hour_same_min() {
-        String username = null;
-        MeetingRoom meetingRoom = null;
-        LocalDate date = null;
-        LocalTime startTime = LocalTime.of(0, 0);
-        LocalTime endTime = LocalTime.of(1, 0);
+    public void isEndTimeLaterThanStartTime() {
+        LocalTime endTime = startTime.plusMinutes(30);
         Reservation reservation = new Reservation(username, meetingRoom, date, startTime, endTime);
         assertThat(reservation).isNotNull();
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void isEndTimeLaterThanStartTime_same() {
-        String username = null;
-        MeetingRoom meetingRoom = null;
-        LocalDate date = null;
-        LocalTime startTime = LocalTime.of(0, 0);
-        LocalTime endTime = LocalTime.of(0, 0);
+        LocalTime endTime = startTime;
         new Reservation(username, meetingRoom, date, startTime, endTime);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void isEndTimeLaterThanStartTime_earlier() {
-        String username = null;
-        MeetingRoom meetingRoom = null;
-        LocalDate date = null;
-        LocalTime startTime = LocalTime.of(1, 0);
-        LocalTime endTime = LocalTime.of(0, 0);
+        LocalTime endTime = startTime.minusMinutes(30);
         new Reservation(username, meetingRoom, date, startTime, endTime);
     }
 }
