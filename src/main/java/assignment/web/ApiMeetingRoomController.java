787fdb5c599b9fb6a49c2fc3a8e7b3f1a5fdce9b
Kelicia91 <kelicia91@gmail.com>
2018-10-15 20:33:13 +0900

    chore : REST API 반환타입 ResponseEntity로 통일

--- src/main/java/assignment/web/ApiMeetingRoomController.java
+++ src/main/java/assignment/web/ApiMeetingRoomController.java
@@ -4,6 +4,7 @@ import assignment.domain.MeetingRoom;
 import assignment.service.MeetingRoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,7 @@ public class ApiMeetingRoomController {
 
     @GetMapping
     @Cacheable("meetingRooms")
-    public List<MeetingRoom> get() {
-        return meetingRoomService.findByAll();
+    public ResponseEntity<List<MeetingRoom>> get() {
+        return ResponseEntity.ok(meetingRoomService.findByAll());
     }
 }
